4384b2e2951ef6a5cb61f01eb4755692b4e128c5
Matt Mitchell <goodieboy@gmail.com>
2011-03-27 21:54:49 -0400

    added config switching to ENV if the file is not found

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,3 +1,10 @@
-facebook_settings = YAML.load_file(Rails.root.join('config', 'facebook.yml'))[Rails.env]
+fb_config_file = Rails.root.join('config', 'facebook.yml')
+
+if File.exists? fb_config_file
+  facebook_settings = YAML.load_file(fb_config_file)[Rails.env]
+elsif
+  facebook_settings = {"app_id" => ENV["app_id"], "secret_key" => ENV["secret_key"]}
+end
+
 FACEBOOK_APP_ID = facebook_settings['app_id']
 FACEBOOK_SECRET_KEY = facebook_settings['secret_key']
