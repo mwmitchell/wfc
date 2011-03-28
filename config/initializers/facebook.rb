e6174c0ac95b097acc64fa2765dbf133f40317b7
Matt Mitchell <goodieboy@gmail.com>
2011-03-27 22:00:15 -0400

    oops

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -3,7 +3,7 @@ fb_config_file = Rails.root.join('config', 'facebook.yml')
 if File.exists? fb_config_file
   facebook_settings = YAML.load_file(fb_config_file)[Rails.env]
 elsif
-  facebook_settings = {"APP_ID" => ENV["app_id"], "SECRET_KEY" => ENV["secret_key"]}
+  facebook_settings = {"app_id" => ENV["APP_ID"], "secret_key" => ENV["SECRET_KEY"]}
 end
 
 FACEBOOK_APP_ID = facebook_settings['app_id']
