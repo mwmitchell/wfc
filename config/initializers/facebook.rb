770e18d06e4dbd0e5df07f27a816b19117399e2d
Matt Mitchell <goodieboy@gmail.com>
2011-03-27 21:55:52 -0400

    changed config var case

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -3,7 +3,7 @@ fb_config_file = Rails.root.join('config', 'facebook.yml')
 if File.exists? fb_config_file
   facebook_settings = YAML.load_file(fb_config_file)[Rails.env]
 elsif
-  facebook_settings = {"app_id" => ENV["app_id"], "secret_key" => ENV["secret_key"]}
+  facebook_settings = {"APP_ID" => ENV["app_id"], "SECRET_KEY" => ENV["secret_key"]}
 end
 
 FACEBOOK_APP_ID = facebook_settings['app_id']
